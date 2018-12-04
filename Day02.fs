13ba0a8c0f80430d730a4be5d26cec88a839b148
Christoffer Lette <christoffer.lette@gmail.com>
2018-12-04 23:35:50 +0100

    Days 1, 2 and 3.

---
+++ Day02.fs
@@ -0,0 +1,309 @@
+module Day02
+    open System
+
+    let d =
+        "ovfclbidieyujztrswxmckgnaw
+pmfqlbimheyujztrswxmckgnap
+ovfqlbidhetujztrswxmcfgnas
+gvfqebddheyujztrswxmckgnap
+ovfqlbidheyejztrswxqekgnap
+ovzqlbiqheyujztsswxmckgnap
+oofqlbidhoyujztoswxmckgnap
+ovfqlbicqeyujztrswxmckgncp
+ovfelbidheyujltrswxmcwgnap
+ovfqlbidheyujzerswxmchgnaf
+bvfqlbidheyxjztnswxmckgnap
+ovfqlbidheyugztrswamnkgnap
+ovfqxbidheyujrtrswxmckgbap
+ovfqlbidheyujztrdwxqckgjap
+ovfqebiqheyujztrscxmckgnap
+avfqlbidheyvjztkswxmckgnap
+ovfqlbidheyujktrswxvskgnap
+ovfqlbidheeujztrswrmckgnae
+ovaqlbidheydjztrswxmchgnap
+ovfqlbodweyujztpswxmckgnap
+xvfqlbidhmyujztrswxmykgnap
+ovfqlnidheyujztxswumckgnap
+ovfqlbidhexujztrswxyckgeap
+ovfqlkidhekubztrswxmckgnap
+ovfqlbidheysjzkrsxxmckgnap
+oxfqebidheyujzprswxmckgnap
+ovfqlbidhetujztrswmmckghap
+ovfclbidhuyujztrswrmckgnap
+ovfqlbijhdyujztrswxmcvgnap
+ovfqlkidhyyujztrswxvckgnap
+ovfqlbiehlyujztrswxhckgnap
+ovfqlbidheyxjjtrsdxmckgnap
+jvfqlbidheyujztrvwxmckcnap
+ovfvlbidheyujzhrswxmckgnzp
+ovfqnbidhuyujztrswfmckgnap
+ovfrlbidheyujztpswxmckgnat
+ovfqpbidheyujztrywxmcngnap
+ovfqlbidheyumrtrswpmckgnap
+ovfqlbidhoyzjztrswxmckgkap
+ovfqlbibheyuhztrswxmskgnap
+ovfqlbidheybjzfrswxkckgnap
+ovfqnbinheyujztrawxmckgnap
+ovfqlbidheyujztryxxmckgnao
+ovfqzbidheyujztrsuxmckgnpp
+ovfqlbidherujztrswxmckgjsp
+ovfqlbidheyujhtrywxmckgtap
+oofmlbidheyujftrswxmckgnap
+ovfqlbidhhyujztrawxmckgnbp
+ovfqlbidheyujztrswxeckmnae
+lvfqlbidheyujztrswxzckvnap
+ovfqlbidheyujztrswxmckqnfr
+offqlbidheyrjztrswxmwkgnap
+ovnqlbidzeyujztmswxmckgnap
+ovfxlbxdheyujztrawxmckgnap
+ovfqmbidheyujztrsaxwckgnap
+ovfqlbidhryzjztrswxmckgcap
+offqlbidheyujnthswxmckgnap
+ogmqlbimheyujztrswxmckgnap
+ovfqlbidheyulztkswxockgnap
+ovfqlbidheyujjtrswxmckypap
+ovfqibidheypjztrswxmskgnap
+ovfqlbwdhyyujztrswxmckgnnp
+ovfqlbidheyujztsvwxmckgkap
+odfqlbidoeyujztrswxjckgnap
+ovfqlbodpeyujztrswxmcggnap
+ovfqlbicheyujztrhwxmcagnap
+ovfqlbidheyuaztrgwxmckhnap
+ovfwlbidhyyujztrswtmckgnap
+ovfqlbidgzyujztrswxmckgcap
+ovnqlbcdheyujztrswxmckgnup
+ovfqlbieheyujrtrsdxmckgnap
+ovfqlbidkeyujztrswfmckgnqp
+ovfqlbidtekujztrswxsckgnap
+ovfklbedheyujztrscxmckgnap
+ovfqltivhnyujztrswxmckgnap
+ovfqlbidheyuvuyrswxmckgnap
+ovfqlbidheyjjrtrcwxmckgnap
+ojfqlbidheyujztrswxmckguvp
+ovfqlbidheiqjqtrswxmckgnap
+ivfqlfidheyujatrswxmckgnap
+cvfqlbidheyujgtrswxmckgnrp
+ovfclbidheeujztrswxmckgnaw
+ovfqlbhdheyujztrswvmcklnap
+ovfqcbidheyvjztaswxmckgnap
+ovgqlbijheyujztrswxvckgnap
+gvfqlbidheyujvtrswxmckgnaj
+ovfqlbidheyujztrdwxmcggnvp
+cvfqlbidheyujgtrswxmckqnap
+ovfqlbrdheyqjztrswxmckgnaj
+ovfqlbidheyujzjrswbmcrgnap
+ovfqlbvdheyujxtrswxvckgnap
+ovaqlbidheyujctrswxmbkgnap
+ovfqlbidheyujztgdwxvckgnap
+ovfqlbidhevujztrssxmwkgnap
+rvfqlbidheyujztrzwxmckhnap
+ovfqmbidheysjztrswxmikgnap
+ovfqlbidheiujztrsdxuckgnap
+ovfqlbidheyveztrswxmckgnah
+ovfqnbiaheytjztrswxmckgnap
+ovfqlbidnayujhtrswxmckgnap
+ovfqlbidheyujztnswxdckgnag
+ovfqlbidheyuyztrswxmzzgnap
+ovfqlbiohexujzthswxmckgnap
+lvfqlbidheyujztcswxxckgnap
+ovuqlbidhfxujztrswxmckgnap
+ovfqluidheyujotrswxmrkgnap
+ovfalbidheyujztrswxhckgngp
+ohjqlbidheyujztrswumckgnap
+ovfqxbidhecujztrspxmckgnap
+ovfqcbidheyusztrpwxmckgnap
+fvfwlbidheyujztrswxmcxgnap
+ovfqlbidhxyplztrswxmckgnap
+ovfqlbidheyujftrswxdckgrap
+ovfqlepdheyujztrswxmckgnjp
+ojjqlbidhuyujztrswxmckgnap
+ovfqlbwdheyujztrswxmcggeap
+ovfqlbidheyujltrscxkckgnap
+oifqibidheyujztrswxjckgnap
+ovfqlbigheyujztrswdmcqgnap
+ovfqlbieheyujztrswxzzkgnap
+ovfqlbidheyujztrswmmcgbnap
+ovfqlbidhnyujzerswxmkkgnap
+ovfqdbinheyujztrswxeckgnap
+oveqlbidheyujztrswhmckgnab
+ovfqkbytheyujztrswxmckgnap
+ovfqlbidheyujstsswxmcklnap
+ovfimbidheyujztrewxmckgnap
+ovfqebidhequjztrnwxmckgnap
+ovfqlbidheyukztrswxmckunwp
+oifqlbidheyuwztrswxmckgnao
+ovfqlbidweyufztrswxmckgtap
+evfqlbidheyujztrswxsckvnap
+svbqlbidheyujztrsaxmckgnap
+ovfqlbidheyaoztrswxmckjnap
+ovfqllidheyujztrswxmckynhp
+ohfqlbidheyujatrswtmckgnap
+omfjlfidheyujztrswxmckgnap
+xvfqlbidheyujutrswxvckgnap
+ovfqlbidheyukztsswxmzkgnap
+ovfqibidhehujztrswxeckgnap
+ovfqlbydheyuoztrswxmcygnap
+ovfqlbidheyufztrmwxmckvnap
+ovfqrbkdheyujztrswxmckgnaq
+ovfqlbigheyuyztrswxmckgzap
+ovfqlbidheyujztrsjxmcnnnap
+uvfqlbipheyujztrswxmckgnay
+ovfqlbddneyujbtrswxmckgnap
+tvfqlbidheyujztrswxpckgeap
+ovfqlbidheyuiztrhwxmckznap
+ovfqlbidheyujzteswxvckgvap
+avfqlbidheyijzlrswxmckgnap
+oqfqmbidheyujvtrswxmckgnap
+ovnqlbidneyujztrswxmckxnap
+ovfqlbidfeyujztrswxqckgncp
+ovfaybidheyujztrswxrckgnap
+ovfqlbidhemujzarnwxmckgnap
+ovfqlwidheyujctrsfxmckgnap
+ovtelbidheysjztrswxmckgnap
+ovfqlbidheyujztrswsmchunap
+pvfqlbidheyulztrswxmckynap
+ovfqlbzdhezujztfswxmckgnap
+ozfqibidheyujztrhwxmckgnap
+ovfqlbioheycjztmswxmckgnap
+ovfqleidheyujztoswxmckgnhp
+ovfqlcidhejujztrswnmckgnap
+eqfqlbidheyujztrswxmrkgnap
+ovfqlbitheywjztrmwxmckgnap
+ovfqlbidheyujptrswnmcggnap
+oofqlbidhjyujztrswxmcegnap
+ovfqlbidmeyujztrswxmcxgnxp
+ovjhlbidhefujztrswxmckgnap
+ovfqlbidkeyujzarswxmcugnap
+hvyqlridheyujztrswxmckgnap
+ovfqlbidheyujxtrswqmckgnpp
+ovfqlbidheyuiztrtsxmckgnap
+ovfqlbidqeyuuztrbwxmckgnap
+ovfqpbidheyujztrswxwekgnap
+ovfqltidheyujztrbwxmckgnab
+okfxluidheyujztrswxmckgnap
+ovfplbidheyujztrsaxmckgnax
+wvfqlbidheiujztrswxjckgnap
+ovfqlbidheyqjzlrsqxmckgnap
+ovfqlbadheyujztrsxxmckgnop
+ovfqliidheyujzerswvmckgnap
+ovlrlbidheyujztaswxmckgnap
+cvzqlbidheyujgtrswxmckqnap
+ovfqlbidheyujzuqswxmckgnnp
+ovfqlsjdheyujztrswxmcklnap
+ovrqlbidheyujztrssrmckgnap
+ovcqlbidheyujztrsmxmcognap
+ovcqlbidheyjjztrswxmckunap
+ovfilbrdhnyujztrswxmckgnap
+ovfqlbodheyujztrswxeckqnap
+ovfqlbidhuyujqtrswxdckgnap
+ovmqlbidheyujderswxmckgnap
+ovfylbidheyajzrrswxmckgnap
+ovfklbidhtyujzjrswxmckgnap
+rvfqlbidheyujztcswxcckgnap
+ovfnlyidheyuwztrswxmckgnap
+ovfqlbidhexujztrswxmpktnap
+ovfplbidheyfjztrswhmckgnap
+oyfqlbidmexujztrswxmckgnap
+mvfqlbidhcyujztrawxmckgnap
+ovfqlbidhqyujdtrswxmcbgnap
+ovfqjbidheybjrtrswxmckgnap
+ozfqlbidhbyujztrswxmckgpap
+okfqlbidheyujztrmwxmckhnap
+ovfqlbydheyujzrrswxnckgnap
+ovfqtbidheycjztrswxmckgnah
+avfqloidheyujztrswxyckgnap
+ovfqlbldteyujzyrswxmckgnap
+ovfqlbpdhedujztpswxmckgnap
+ovfqlbidheyujztrswxucrvnap
+ocnqlbidheyujztrswxmwkgnap
+ovfqlvidheyujztrswkmckgnlp
+evfqlbidheyujzmrswqmckgnap
+ovfqlbidhryujztrcwxmekgnap
+ovfqlbvdheyujztrzwxmcxgnap
+ovfqlridgeyujztrswxmkkgnap
+yvfqlbidheyujzthswzmckgnap
+ovfqlbidheyujmtrswxyukgnap
+ovfqlbidheqgjztrswdmckgnap
+dvfzlcidheyujztrswxmckgnap
+jvfqlbidheyujztrswxmczgnae
+ovfqlbzdheyujztrswxyckgnjp
+ovfqlbxdheyujatrswxmcqgnap
+ovftlbldheyujztrewxmckgnap
+owfqlbitheyujzyrswxmckgnap
+ovfqlbidheyujztrswxmchgtvp
+ovfqibidheyujzttswxmkkgnap
+ovkqlbodheyujztvswxmckgnap
+onfqlbbdheyujztrxwxmckgnap
+ovfqlbidyeysgztrswxmckgnap
+ovfqlbixherujztrswxmcngnap
+ovlqlbidfeyujztrswxgckgnap
+ovfqlbfdheyujztwswumckgnap
+ovfqlbidheeujztrswxmckgkah
+ovfqtbqdheyujztrswmmckgnap
+bbfqlbigheyujztrswxmckgnap
+ovfqljidheyujztrswxmwkgnip
+ovfqobidheyujztrsvxmmkgnap
+ovfqlbidheydjvtrswxmckanap
+ovfqlxidheyujztrswgmckgnep
+jvfqlbidhzyujztrswxmckgnay
+ovfqlbidhkyujztrswxmlkenap
+ovfqobidheyujztrswxmckjnaf
+ovfxlbidheyujztrswxmcbgnac
+ovfqcbidhtyujztrswxmckqnap
+ozfglbidheyujzvrswxmckgnap
+ovfqlbidheyujztoswxyckcnap"
+
+    let xs =
+        d.Split Environment.NewLine
+        |> Array.toList
+
+    let bleh (s : string) =
+        let ks =
+            s
+            |> Seq.toList
+            |> Seq.groupBy id
+            |> Seq.groupBy (fun (_, v) -> Seq.length v)
+            |> Seq.map (fun (k, _) -> k)
+            |> Set.ofSeq
+        
+        (if Set.contains 2 ks then 1 else 0), if Set.contains 3 ks then 1 else 0
+
+    let prod () =
+        xs
+        |> List.map bleh
+        |> List.reduce (fun (a, b) (c, d) -> (a + c, b + d))
+        |> (fun (x, y) -> x * y)
+
+    let diff () =
+
+        let separation (s1 : string) (s2 : string) =
+            let cs1 = s1 |> Seq.toList
+            let cs2 = s2 |> Seq.toList
+
+            let charPairs = List.zip cs1 cs2
+
+            let charPairsWithSep =
+                charPairs
+                |> List.map (fun (c1, c2) -> (c1, (int c1) - (int c2)))
+
+            let numberOfCharsThatDiffer =
+                charPairsWithSep
+                |> List.where (fun (_, s) -> s <> 0)
+                |> List.length
+
+            (numberOfCharsThatDiffer, charPairsWithSep)
+
+        let (_, info) =
+            List.allPairs xs xs
+            |> List.map (fun (s1, s2) -> separation s1 s2)
+            |> List.find (fun (s, _) -> s = 1)
+
+        info
+            |> List.where (fun (_, x) -> x = 0)
+            |> List.map (fun (c, _) -> c)
+            |> List.fold (fun acc curr -> acc + (string curr)) ""
+
+    let show () =
+        printfn "Day 2:"
+        printfn "   Prod: %i" (prod ())
+        printfn "   Diff: %s" (diff ())
---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿[<EntryPoint>]
+let main argv =
+
+    //Day01.show ()
+    //Day02.show ()
+    Day03.show ()
+
+    0
