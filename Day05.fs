ab150b52bbec310914fd13f159e63a5fa93e1cdb
Christoffer Lette <christoffer.lette@gmail.com>
2018-12-08 13:53:44 +0100

    Day 5.

---
+++ Day05.fs
@@ -0,0 +1,49 @@
+module Day05
+    open System
+
+    let xs = Day05Data.d |> Seq.toList
+
+    let (|IsReducible|IsNotReducible|) (a, b) =
+        if ((a <> b) && ((Char.ToLower a) = (Char.ToLower b))) then
+            IsReducible
+        else
+            IsNotReducible
+
+    let reduce xs =
+        let rec inner ys (acc, r) =
+            match ys with
+            | [] -> (acc, r)
+            | [y] -> (y :: acc, r)
+            | a :: b :: cs ->
+                match (a, b) with
+                | IsReducible -> inner cs (acc, r + 1)
+                | IsNotReducible -> inner (b :: cs) (a :: acc, r)
+
+        let rec outer ys =
+            match inner ys ([], 0) with
+            | (acc, 0) -> acc
+            | (acc, _) -> outer acc
+
+        outer xs
+
+    let part1 () =
+        reduce xs
+        |> List.length
+
+    let part2 () =
+        let polymerTypes =
+            xs
+            |> List.fold (fun acc curr -> acc |> Set.add (curr |> Char.ToLower)) Set.empty
+            |> Set.toList
+
+        let removePolymerType t ps =
+            ps |> List.filter (fun p -> (p |> Char.ToLower) <> t)
+
+        polymerTypes
+        |> List.map (fun t -> removePolymerType t xs |> reduce |> List.length)
+        |> List.min
+
+    let show () =
+        printfn "Day 5:"
+        printfn "   Part 1: %A (11476)" (part1 ())
+        printfn "   Part 2: %A (5446)" (part2 ())
--- Program.fs
+++ Program.fs
@@ -4,6 +4,7 @@ let main argv =
     //Day01.show ()
     //Day02.show ()
     //Day03.show ()
-    Day04.show ()
+    //Day04.show ()
+    Day05.show ()
 
     0
