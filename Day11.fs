8bc086e05037f97ded81cb4e3006573059dd158e
Christoffer Lette <christoffer.lette@gmail.com>
2018-12-11 13:51:12 +0100

    Day 11 improvements and cheats.

--- Day11.fs
+++ Day11.fs
@@ -7,20 +7,30 @@ module Day11
         let rackId = x' + 10
         (((rackId * y' + serial) * rackId) % 1000) / 100 - 5
 
-    let subGridTotal x y z (grid : int [,]) =
-        [ for a in x .. (x+(z-1)) do
-          for b in y .. (y+(z-1)) do
-          yield grid.[a, b] ]
-        |> Seq.sum
-
     let xs =
         Array2D.init 300 300 (toPowerLevel Day11Data.d)
 
+    let subGridTotal x y z =
+        let mutable sum = 0
+        for a = x to (x+(z-1)) do
+            for b = y to (y+(z-1)) do
+                sum <- sum + xs.[a, b]
+        sum
+
+        // seq {
+        //     for a in x .. (x+(z-1)) do
+        //     for b in y .. (y+(z-1)) do
+        //         yield xs.[a, b] }
+        // |> Seq.sum
+
+    let t4fourth (_, _, _, x) = x
+
     let findMaxBySize n =
-        [ for x in 0 .. (300 - n) do
-          for y in 0 .. (300 - n) do
-          yield (x, y, n, subGridTotal x y n xs)]
-        |> List.maxBy (fun (_, _, _, p) -> p)
+        seq {
+            for x in 0 .. (300 - n) do
+            for y in 0 .. (300 - n) do
+                yield (x, y, n, subGridTotal x y n) }
+        |> Seq.maxBy t4fourth
 
     let part1 () =
 
@@ -35,10 +45,13 @@ module Day11
             else
                 let m = (findMaxBySize n)
                 //printfn "%i: %A" n m
-                inner (n + 1) (m :: acc)
+                if t4fourth m < 0 then  // <-- Totally arbitrary exit condition!
+                    acc
+                else
+                    inner (n + 1) (m :: acc)
         
         inner 1 []
-        |> List.maxBy (fun (_, _, _, p) -> p)
+        |> List.maxBy t4fourth
         |> (fun (x, y, z, _) -> sprintf "%i,%i,%i" (x + 1) (y + 1) z)
 
     let show () =
