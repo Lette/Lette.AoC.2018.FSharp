5c7c050b6893f589dfba6f00d133e0ebb0b69bcd
Christoffer Lette <christoffer.lette@gmail.com>
2018-12-18 13:05:02 +0100

    Cleanup in Day 18.

--- Day18.fs
+++ Day18.fs
@@ -20,59 +20,65 @@ module Day18
 
     let getAdjacentCells map x y =
         let getCell' = getCell map
-        seq {
-            yield getCell' (x - 1) (y - 1)
-            yield getCell' (x) (y - 1)
-            yield getCell' (x + 1) (y - 1)
-            yield getCell' (x - 1) (y)
-            yield getCell' (x + 1) (y)
-            yield getCell' (x - 1) (y + 1)
-            yield getCell' (x) (y + 1)
-            yield getCell' (x + 1) (y + 1)
-        }
+        
+        [
+            getCell' (x - 1) (y - 1)
+            getCell' (x) (y - 1)
+            getCell' (x + 1) (y - 1)
+            getCell' (x - 1) (y)
+            getCell' (x + 1) (y)
+            getCell' (x - 1) (y + 1)
+            getCell' (x) (y + 1)
+            getCell' (x + 1) (y + 1)
+        ]
 
     let showMap (map : char [] []) =
+        System.Console.SetCursorPosition (0, 0)
+        map
+        |> Array.map (Array.toSeq >> String.Concat)
+        |> Array.iter (printfn "%s")
         printfn ""
-        map |> Array.map (Array.toSeq >> String.Concat) |> Array.iter (printfn "%s")
-        printfn ""
+
+    let tickCell map x y =
+        let cell = getCell map x y
+        let adjacentCells = getAdjacentCells map x y
+
+        match cell with
+        | '.' -> if adjacentCells |> List.filter ((=) '|') |> List.truncate 3 |> List.length >= 3 then '|' else '.'
+        | '|' -> if adjacentCells |> List.filter ((=) '#') |> List.length >= 3 then '#' else '|'
+        | '#' -> if adjacentCells |> List.groupBy id |> List.map (fun (k, _) -> k) |> (fun cs -> (cs |> List.contains '#') && (cs |> List.contains '|')) then '#' else '.'
+        | _   -> failwithf "could not parse input: %A" cell
+
+    let tickRow map y =
+        Array.init dx (flip (tickCell map) y)
 
     let tick map =
+        Array.init dy (tickRow map)
 
-        let createCell map x y =
-            let cell = getCell map x y
-            let adjacentCells = getAdjacentCells map x y
-
-            match cell with
-            | '.' -> if adjacentCells |> Seq.filter ((=) '|') |> Seq.length >= 3 then '|' else '.'
-            | '|' -> if adjacentCells |> Seq.filter ((=) '#') |> Seq.length >= 3 then '#' else '|'
-            | '#' -> if adjacentCells |> Seq.groupBy id |> Seq.map (fun (k, v) -> k) |> (fun cs -> (cs |>Seq.contains '#') && (cs |> Seq.contains '|')) then '#' else '.'
-            | _   -> failwithf "could not parse input: %A" cell
-
-        let createRow map y =
-            Array.init dx (flip (createCell map) y)
-
-        Array.init dy (createRow map)
-
-    let rec run map n =
-        match n with
-        | 0 -> map
-        | _ -> run (tick map) (n - 1)
-
-    let runAll n =
-        let map' = run initialMap n
-
-        let allCells = map' |> Array.concat |> Array.toSeq |> Seq.cache
+    let product map =
+        let allCells = map |> Array.concat |> Array.toSeq |> Seq.cache
         let woodedAcres = allCells |> Seq.filter ((=) '|') |> Seq.length
         let lumberyards = allCells |> Seq.filter ((=) '#') |> Seq.length
 
         woodedAcres * lumberyards
 
+    let rec run map n =
+        match n with
+        | 0 -> map
+        | _ ->
+            let map' = tick map
+            
+            run map' (n - 1)
+
+    let runAll n =
+        run initialMap n
+        |> product
+
     let part1 () =
         runAll 10
 
     let part2 () =
-        //runAll 1000000000
-        0
+        runAll 1000000000
 
     let show () =
         showDay
